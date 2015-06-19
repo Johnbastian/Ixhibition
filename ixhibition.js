4591891def8e60c6ce191ce24f71cc482db75ca9
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2015-06-19 22:03:19 +0100

    Further automation and generating code

--- ixhibition.js
+++ ixhibition.js
@@ -1,3 +1,14 @@
+var main_styling = "#ixhibition{/* overflow: hidden !important; */ padding: 0px !important;} #ixb_listcontainer{ height: 100%; width: 100%;    }";
+
+main_styling += ".ixb_images{       \
+    height: 100%; width: 100%;  \
+    background-size: contain; background-repeat: no-repeat; background-position: center center; \
+    }";
+
+document.getElementById("ixb_main").innerHTML = main_styling;
+
+var transition_type = "vertical-reverse"; //vertical, vertical-reverse, horizontal, horizontal-reverse, stack
+
 var urlList = [
     "images/EarthMoon.jpg"
     , "images/Fuji.jpg"
@@ -5,7 +16,15 @@ var urlList = [
     , "images/Bahamas.jpg"
 ];
 
-var transition_type = "vertical"; //vertical-reverse, horizontal, horizontal-reverse, stack
+var imageListHTML = "";
+for (var ulCounter = 0; ulCounter < urlList.length; ulCounter++) {
+    imageListHTML += "<div id='ixb_wrapper" + ulCounter + "' class='ixb_wrapper'>";
+    imageListHTML += "<div id='ixb_image" + ulCounter + "' class='ixb_images' style='background-image: url(" + urlList[ulCounter] + ");'></div>";
+    imageListHTML += "</div>";
+}
+
+document.getElementById("ixhibition").innerHTML = "<div id='ixb_listcontainer'>" + imageListHTML + "<div>";
+
 
 var display_time = 4,
     phaseIn_duration = 1,
@@ -13,7 +32,8 @@ var display_time = 4,
     transition_overlap = 0.5,
     loopCount = "3";
 
-var fullPhase_duration = display_time + phaseIn_duration + phaseOut_duration;
+var fullPhase_duration = display_time + phaseIn_duration + phaseOut_duration,
+    transition_duration = phaseOut_duration + phaseIn_duration - transition_overlap;
 
 var totalTime = urlList.length * (fullPhase_duration - transition_overlap);
 console.log("totalTime time is: " + totalTime);
@@ -42,15 +62,15 @@ var fadeIn = true,
     fadeOut = true;
 
 var phaseIn_animations = [
-    {"transform": "rotateY(90deg)"},
+    {"transform": "rotateX(90deg)", "transform-origin" : "top center"},
     //{"transform": "rotateY(20deg)"},
-    {"transform": "rotateY(0deg)"}
+    {"transform": "rotateX(0deg)"}
 ];
 
 var phaseOut_animations = [
-    {"transform": "rotateY(0deg)"},
+    {"transform": "rotateX(0deg)", "transform-origin" : "bottom center"},
     //{"transform": "rotateY(-20deg)"},
-    {"transform": "rotateY(-90deg)"}
+    {"transform": "rotateX(-90deg)"}
 ];
 
 var phaseIn_length = phaseIn_animations.length,
@@ -131,49 +151,85 @@ processKeyFrames();
 
 
 
-/*
-var transition_animation =
-    "   \
-        @keyframes ixbTransition{    \
-            0%      {opacity: 0;}   \
-            4.54545454%      {opacity: 1;}   \
-            22.72727272%     {opacity: 1;}   \
-            27.27272727%     {opacity: 0;}   \
-            100%    {opacity: 0;}   \
-        }   \
-        .ixb_images{ \
-            perspective-origin: center center; \
-            opacity: 0; \
-            animation: ixbTransition;        \
-            animation-duration: " + 22 + "s;    \
-            animation-timing-function: ease-in-out; \
-            animation-iteration-count: 3;   \
-        }   \
-    ";
-*/
-
-var transition_animation =
+var animation_css =
     "   \
         @keyframes ixbTransition{   " + keyframeValues + "    }   \
         .ixb_images{ \
-            perspective-origin: center center; \
-            opacity: 0; \
+            " + (fadeIn ? "opacity: 0;" : "") + " \
             animation: ixbTransition;        \
             animation-duration: " + totalTime + "s;    \
             animation-iteration-count: " + loopCount + ";   \
         }   \
     ";
 
-
-
-
-
-
-var animation_delays = "";
-for (var dlCounter = 0; dlCounter < delayList.length; dlCounter++) {
-    animation_delays += "#ixb_image" + dlCounter + "{ animation-delay: " + delayList[dlCounter] + "s}\n";
+function getSlideTransition() {
+    switch (transition_type) {
+        case "vertical":
+            return [
+                "transform: translate(0px, 100%)",
+                "transform: translate(0px, 0%)",
+                "transform: translate(0px, -100%)"
+            ];
+            break;
+        case "vertical-reverse":
+            return [
+                "transform: translate(0px, -100%)",
+                "transform: translate(0px, 0%)",
+                "transform: translate(0px, 100%)"
+            ];
+            break;
+        case "horizontal":
+            return [
+                "transform: translate(100%, 0px)",
+                "transform: translate(0%, 0px)",
+                "transform: translate(-100%, 0px)"
+            ];
+            break;
+        case "horizontal-reverse":
+            return [
+                "transform: translate(-100%, 0px)",
+                "transform: translate(0%, 0px)",
+                "transform: translate(100%, 0px)"
+            ];
+            break;
+        default:
+            return [
+                "",
+                "",
+                ""
+            ];
+            break;
+    }
 }
 
 
-document.getElementById("ixb_animation").innerHTML = transition_animation;
-document.getElementById("ixb_delays").innerHTML = animation_delays;
+var transition_percentage = transition_duration * 100 / totalTime,
+    transitionTransforms = getSlideTransition();
+var transition_css =
+    "   \
+        @keyframes xibSlide{    \
+            0%  {" + transitionTransforms[0] + "}  \
+            " + transition_percentage + "%  {" + transitionTransforms[1] + "} \
+            " + (transition_percentage + display_percentage) + "%   {" + transitionTransforms[1] + "}  \
+            " + (transition_percentage + display_percentage + transition_percentage) + "%   {" + transitionTransforms[2] + "}  \
+            100%    {}  \
+        }   \
+        .ixb_wrapper{   \
+            position: absolute; top: 0px; left: 0px; height: 100%; width: 100%; \
+            animation: xibSlide; animation-duration: 22s; animation-iteration-count: 3; animation-timing-function: ease-in-out; \
+        }   \
+    ";
+
+
+
+
+
+var animation_delays = "",
+    transition_delays = "";;
+for (var dlCounter = 0; dlCounter < delayList.length; dlCounter++) {
+    animation_delays += "#ixb_image" + dlCounter + "{ animation-delay: " + delayList[dlCounter] + "s}\n";
+    transition_delays += "#ixb_wrapper" + dlCounter + "{ animation-delay: " + (delayList[dlCounter] - (transition_duration - phaseIn_duration)) + "s}\n";
+}
+
+document.getElementById("ixb_animation").innerHTML = animation_css + "\n" + transition_css;
+document.getElementById("ixb_delays").innerHTML = animation_delays + "\n" + transition_delays;
