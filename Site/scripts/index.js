967f9f587edf48eba0f5ba46b5a63ffa3e8bb106
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2015-07-15 21:56:39 +0100

    Site 11

--- Site/scripts/index.js
+++ Site/scripts/index.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
 	/* Initial Load - load homepage by simulation of clicking on home*/ /* Need to validate for redirection to login page*/
 	$("#homeLink").click();
 
+
     $("#logo").click(function(){    $("#homeLink").click(); });
 
 });
